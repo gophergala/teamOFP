5dad3a55d72526429a8f3c780d9348da37541ac5
Marcelo Silveira <marcelo@mhfs.com.br>
2015-01-24 15:48:28 -0800

    add search hook to angular app

--- api/public/javascript/main.js
+++ api/public/javascript/main.js
@@ -2,9 +2,15 @@ var Groupify = angular.module('Groupify', []);
 
 Groupify.controller('MainCtrl', function($scope, $http) {
   $scope.queue = [];
+  $scope.query = "";
 
   $http.get('/api/v1/queue/list')
   .then(function(res){
     $scope.queue = res.data;
   });
+
+  $scope.search = function(){
+    // TODO bring spotify search logic here
+    console.log($scope.query);
+  }
 });
