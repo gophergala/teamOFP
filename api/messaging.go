7cc561c28054e18bbb092ace694598ebf41a1b22
Ali Hamidi <ahamidi@me.com>
2015-01-24 14:17:20 -0800

    Add pushMessage to messaging

--- api/messaging.go
+++ api/messaging.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/crowdmob/goamz/sqs"
 	"log"
 )
+
+type NotificationMessage struct {
+	Event string `json:"event"`
+	Value string `json:"values"`
+}
 
 func listenOnQueue(q *sqs.Queue, ch chan *sqs.Message) {
 
@@ -23,8 +29,47 @@ func listenOnQueue(q *sqs.Queue, ch chan *sqs.Message) {
 
 func processQueue(ch chan *sqs.Message) {
 	for m := range ch {
-		log.Println("Processing Message: ", m)
+
+		n := &NotificationMessage{}
+
+		if err := json.Unmarshal([]byte(m.Body), &n); err != nil {
+			log.Panic(err)
+		}
+
+		switch n.Event {
+		case "track_end":
+			log.Println("Song Ended")
+			// TODO:
+			// - Get next song from queue
+			// - Push to remote
+
+		case "track_start":
+			log.Println("Song Started")
+
+		case "player_pause":
+			log.Println("Player Paused")
+
+		case "player_start":
+			log.Println("Player Start")
+
+		case "time_left":
+			log.Println("Time Left: ", n.Value)
+
+		}
 	}
 }
 
-//func pushMessage()
+func pushMessage(q *sqs.Queue, message interface{}) error {
+
+	j, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	_, err = q.SendMessage(string(j))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
