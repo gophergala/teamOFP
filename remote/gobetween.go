bf80d71900d7fab656e1d64e8dada6ea4ba4a774
Jonathan Hatalla <jhatalla@jon-mpb.local>
2015-01-24 12:50:35 -0800

    adding parameter for creating commands

--- remote/gobetween.go
+++ remote/gobetween.go
@@ -11,28 +11,31 @@ import (
 const ScriptStart = "tell application \"Spotify\" to "
 
 var commands = map[string]string{
-	"state":    "state",
-	"play":     "play",
-	"pause":    "pause",
-	"stop":     "stop",
-	"duration": "duration of current track",
-	"name":     "name of current track",
-	"album":    "album of current track",
-	"id":       "id of current track",
-	"artwork":  "artwork of current track",
-	"vol_loud": "set sound volume to 100",
-	"vol_soft": "set sound volume to 20",
-	"vol_norm": "set sound volume to 50",
+	"state":      "state",
+	"play":       "play",
+	"pause":      "pause",
+	"stop":       "stop",
+	"duration":   "duration of current track",
+	"name":       "name of current track",
+	"album":      "album of current track",
+	"id":         "id of current track",
+	"artwork":    "artwork of current track",
+	"vol_loud":   "set sound volume to 100",
+	"vol_soft":   "set sound volume to 20",
+	"vol_norm":   "set sound volume to 50",
+	"set_volume": "set sound volume to ", //requires parameter
+	"play_track": "play track ",          //requires parameter
 }
 
-func createSystemCall(command string) string {
-	return ScriptStart + commands[command]
+func createSystemCall(command string, param string) string {
+	return ScriptStart + commands[command] + param
 }
+
 func main() {
 	var cmd = flag.String("o", "pause", "Enter the command for spotify")
 	flag.Parse()
 
-	command := createSystemCall(*cmd)
+	command := createSystemCall(*cmd, "")
 	if command == "" {
 		fmt.Println("exiting...")
 	}
