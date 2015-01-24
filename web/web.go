02d12e17ed40e0d81334cdc0539aebced50d35c5
Marcelo Silveira <marcelo@mhfs.com.br>
2015-01-24 12:09:46 -0800

    add dummy track response

--- web/web.go
+++ web/web.go
@@ -1,10 +1,29 @@
 package main
 
 import (
-	"github.com/codegangsta/negroni"
+	"encoding/json"
 	"net/http"
 	"os"
+
+	"github.com/codegangsta/negroni"
 )
+
+type Track struct {
+	Id     string `json:"id"`
+	Time   string `json:"time"`
+	Name   string `json:"name"`
+	Artist string `json:"artist"`
+	Album  string `json:"album"`
+}
+
+var tracks []Track
+
+func init() {
+	s1 := Track{Time: "3:07", Name: "The Ride", Artist: "David Allan Cole", Album: "16 Biggest Hits"}
+	s2 := Track{Time: "1:20", Name: "Bookends", Artist: "Simon and Garfunkel", Album: "Greatest Hits"}
+	s3 := Track{Time: "3:28", Name: "A Woman Left Lonely", Artist: "Janis Joplin", Album: "The Pearl Sessions"}
+	tracks = []Track{s1, s2, s3}
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -21,9 +40,13 @@ func main() {
 }
 
 func searchAction(w http.ResponseWriter, req *http.Request) {
+	response, _ := json.Marshal(tracks)
+	w.Write(response)
 }
 
 func queueAction(w http.ResponseWriter, req *http.Request) {
+	response, _ := json.Marshal(tracks)
+	w.Write(response)
 }
 
 func enqueueAction(w http.ResponseWriter, req *http.Request) {
